ce002944861a8c33bb4f2a526cdaad1c2751c6f5
vicjuan <vicjuan1003@gmail.com>
2016-06-18 13:41:16 +0800

    Update flickr-gmap.js

--- flickr-gmap.js
+++ flickr-gmap.js
@@ -1,8 +1,8 @@
-document.write('<link type="text/css" media="screen" rel="stylesheet" href="http://flickr-gmap-show-ssl.googlecode.com/svn/trunk/lightbox/css/lightbox.css"/>');
+document.write('<link type="text/css" media="screen" rel="stylesheet" href="http://vicjuan.github.io/flickr-gmap-show-ssl/lightbox/css/lightbox.css"/>');
 
-document.write('<script type="text/javascript" src="http://flickr-gmap-show-ssl.googlecode.com/svn/trunk/gmap2.js"></script>');
-document.write('<script type="text/javascript" src="http://flickr-gmap-show-ssl.googlecode.com/svn/trunk/flickr-gmap-show.js"></script>');
+document.write('<script type="text/javascript" src="http://vicjuan.github.io/flickr-gmap-show-ssl/gmap2.js"></script>');
+document.write('<script type="text/javascript" src="http://vicjuan.github.io/flickr-gmap-show-ssl/flickr-gmap-show.js"></script>');
 
-document.write('<script type="text/javascript" src="http://flickr-gmap-show-ssl.googlecode.com/svn/trunk/lightbox/js/prototype.js"></script>');
-document.write('<script type="text/javascript" src="http://flickr-gmap-show-ssl.googlecode.com/svn/trunk/lightbox/js/scriptaculous.js?load=effects"></script>');
-document.write('<script type="text/javascript" src="http://flickr-gmap-show-ssl.googlecode.com/svn/trunk/lightbox/js/lightbox.js"></script>');
+document.write('<script type="text/javascript" src="http://vicjuan.github.io/flickr-gmap-show-ssl/lightbox/js/prototype.js"></script>');
+document.write('<script type="text/javascript" src="http://vicjuan.github.io/flickr-gmap-show-ssl/lightbox/js/scriptaculous.js?load=effects"></script>');
+document.write('<script type="text/javascript" src="http://vicjuan.github.io/flickr-gmap-show-ssl/lightbox/js/lightbox.js"></script>');
